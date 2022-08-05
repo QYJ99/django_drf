65c9c2f29eccfad218566f471d2b05adcbff8989
git_username <1428398807@qq.com>
2022-08-05 19:54:12 +0800

    更新

--- app02/serializers.py
+++ app02/serializers.py
@@ -99,28 +99,111 @@ from .models import Article,Category
 #         # fields = '__all__'
 #         fields = ('id','name','articles')
 
+# # SlugRelatedField
+# class ArticleSerializer(serializers.ModelSerializer):
+#     category = serializers.SlugRelatedField(
+#         read_only=True,
+#         slug_field='name' # 返回的值
+#     )
+#     class Meta:
+#         model = Article
+#         # fields = ('id', 'vum', 'content', 'title')
+#         fields = '__all__'
+#
+#
+# class CategorySerializer(serializers.ModelSerializer):
+#     articles = serializers.SlugRelatedField(
+#         read_only=True,
+#         slug_field='content',  # 返回的值
+#         many=True,
+#     )
+#     class Meta:
+#         model = Category
+#         # fields = '__all__'
+#         fields = ('id','name','articles')
+
+
 # SlugRelatedField
+# class ArticleSerializer(serializers.ModelSerializer):
+#     category = serializers.HyperlinkedIdentityField(
+#         view_name='app02:category-detail',
+#         # lookup_field='id'
+#     )
+#     class Meta:
+#         model = Article
+#         # fields = ('id', 'vum', 'content', 'title')
+#         fields = '__all__'
+#
+#
+# class CategorySerializer(serializers.ModelSerializer):
+#     articles = serializers.HyperlinkedIdentityField(
+#         view_name='app02:article-detail',
+#         many=True,
+#     )
+#     class Meta:
+#         model = Category
+#         # fields = '__all__'
+#         fields = ('id','name','articles')
+
+# HyperlinkedModelSerializer
+# class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Article
+#         fields = '__all__'
+#
+#         extra_kwargs = {
+#             'url': {'view_name': 'app02:article-detail', 'lookup_field': 'pk'},
+#             'category': {'view_name': 'app02:category-detail', 'lookup_field': 'pk'},
+#         }
+#
+#
+# class CategorySerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Category
+#         fields = ('id', 'name', 'articles','url')
+#
+#         extra_kwargs = {
+#             'url': {'view_name': 'app02:category-detail', 'lookup_field': 'pk'},
+#             'articles': {'view_name': 'app02:article-detail', 'lookup_field': 'pk'},
+#         }
+
+
+# 嵌套序列化关系模型
+# class ArticleSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Article
+#         fields = '__all__'
+#
+#
+# class CategorySerializer(serializers.ModelSerializer):
+#     articles = ArticleSerializer(many=True)
+#
+#     class Meta:
+#         model = Category
+#         fields = '__all__'
+
+# depth
 class ArticleSerializer(serializers.ModelSerializer):
-    category = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='name' # 返回的值
-    )
     class Meta:
         model = Article
-        # fields = ('id', 'vum', 'content', 'title')
         fields = '__all__'
+        depth = 2
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    articles = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='content',  # 返回的值
-        many=True,
-    )
+    articles = ArticleSerializer(many=True)
+
     class Meta:
         model = Category
-        # fields = '__all__'
-        fields = ('id','name','articles')
+        fields = '__all__'
+        # fields = ('id', 'name', 'articles')
+        depth = 2
+
+
+
+
+
+
 
 
 
